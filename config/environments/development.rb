e47c84982c6117a656f88afab43649b6a4528181
agam98 <dwivedi.agam233@gmail.com>
2017-03-19 14:33:27 +0530

    Confirmation Mails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,18 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_cable.disable_request_forgery_protection = true
+
+  Depot::Application.configure do
+    config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "domain.of.sender.net",
+      authentication: "plain",
+      user_name: "dave",
+      password: "secret",
+      enable_starttls_auto: true
+    }
+  end
 end
